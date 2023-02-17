7746354ac92d752708f3df82e1c1aa495c39b5ac
vadim <vadimpoldolin19@gmail.com>
2023-02-17 19:47:06 +0300

    Task 4-8

--- src/Main.java
+++ src/Main.java
@@ -42,14 +42,58 @@ public class Main {
     }
     public static void task4() {
         System.out.println("Задача 4");
+        int age = 14;
+        System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить");
+        if (age > 2 && age <6){
+            System.out.println("в детский сад");
+        } else if ( age> 7 && age<18){
+            System.out.println("в школу");
+        } else if( age> 18 && age<24 ){
+            System.out.println("в университете");
+        } else if( age> 24){
+            System.out.println("на работу");
+        }
     }
     public static void task5() {
         System.out.println("Задача 5");
+        int age=18;
+        System.out.println("Если возраст ребенка равен " + age + ", то ему ");
+        if(age<5){
+            System.out.println("нельзя кататься на аттракционе");
+        } else if(age>5 && age<=14){
+            System.out.println("можно кататься на аттракционе в сопровождении");
+        } else if(age>14){
+            System.out.println("можно кататься без сопровождения взрослого");
+        }
     }
     public static void task6() {
         System.out.println("Задача 6");
+       int ticket=10;
+       System.out.println("Мой номер билета " + ticket);
+       if(ticket>0 && ticket<=60){
+           System.out.println("достанется сидячее место");
+       } else if(ticket>60 && ticket<=102){
+           System.out.println("достанется сидячее место");
+       } else if(ticket>102){
+           System.out.println("место не достанется");
+       }
     }
     public static void task7() {
         System.out.println("Задача 7");
+        int one=1;
+        int two=2;
+        int three=3;
+        boolean whichNumberIsSmaller=one<two && two<three;
+        if(whichNumberIsSmaller){
+            System.out.println(one + " самое маленькаое число из всех");
+        }
+        boolean whichNumberIsAverage=two>one && two<three;
+        if(whichNumberIsAverage){
+            System.out.println(two + " среднее число из всех ");
+        }
+        boolean whichNumberIsBigger= three>one && three>two;
+        if(whichNumberIsBigger){
+            System.out.println(three + " самое большое число из всех");
+        }
     }
 }
